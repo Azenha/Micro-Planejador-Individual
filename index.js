bd1d037993e73773e91b4e28453a1a5c742bc413
Leonardo Azenha <azenha.leonardo@gmail.com>
2024-06-14 20:08:11 -0300

    port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const pug = require("pug");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
